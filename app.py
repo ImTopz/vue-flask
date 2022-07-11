a2f21b0a55b8f6b1965447cf4d4628d24532dd00
zder <1976521709@qq.com>
2022-07-11 23:55:04 +0800

    template

--- app.py
+++ app.py
@@ -1,24 +1,7 @@
-from flask import Flask ,redirect, url_for
-
+from flask import Flask, render_template
 app = Flask(__name__)
-
-#the formula for int/float
-@app.route('/blog/<int:postID>')
-def show_blog(postID):
-   return 'Blog Number %d' % postID
-
-@app.route('/data')
-def data_detect():
-    return 'data test'
-
-
-#url_jump
-@app.route('/user/<name>')
-def hello_user(name):
-   if name =='admin':
-      return redirect(url_for('data_detect'))
-   else:
-      return redirect(url_for('show_blog',postID=3))
-
+@app.route('/hello/<user>')
+def hello_name(user):
+   return render_template('hello.html', name = user)
 if __name__ == '__main__':
-    app.run()
+   app.run(debug = True)
