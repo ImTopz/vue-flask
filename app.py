7eaf56036728d4b7edb06b0378f622235d4087fc
zder <1976521709@qq.com>
2022-07-11 23:28:20 +0800

    flask_demo

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask
+from flask import Flask ,redirect, url_for
 
 app = Flask(__name__)
 
-
-@app.route('/api')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+#the formula for int/float
+@app.route('/blog/<int:postID>')
+def show_blog(postID):
+   return 'Blog Number %d' % postID
 
 @app.route('/data')
 def data_detect():
+    return 'data test'
+
+
+#url_jump
+@app.route('/user/<name>')
+def hello_user(name):
+   if name =='admin':
+      return redirect(url_for('data_detect'))
+   else:
+      return redirect(url_for('show_blog',postID=3))
 
 if __name__ == '__main__':
     app.run()
